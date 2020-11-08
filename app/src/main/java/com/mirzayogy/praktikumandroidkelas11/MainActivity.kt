157103598a6dca6060880b1065675b6fbc1fa658
amukancabbage <amukancabbage@gmail.com>
2020-11-08 11:51:30 +0800

    penambahan view pada activity_main.xml

--- app/src/main/java/com/mirzayogy/praktikumandroidkelas11/MainActivity.kt
+++ app/src/main/java/com/mirzayogy/praktikumandroidkelas11/MainActivity.kt
@@ -2,10 +2,24 @@ package com.mirzayogy.praktikumandroidkelas11
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btLogin = findViewById<Button>(R.id.btLogin)
+
+        btLogin.setOnClickListener {
+            val stEmail = etEmail.text.toString().trim()
+            val stPassword = etPassword.text.toString()
+            val stCombine = stEmail + stPassword
+            Toast.makeText(this,stCombine,Toast.LENGTH_LONG).show()
+        }
     }
 }
